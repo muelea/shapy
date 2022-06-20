caac75d36edce085ce0fa0d6e740bc9a5b78907d
Lea Mueller <lea.mueller@tuebingen.mpg.de>
2022-06-20 22:45:52 +0200

    download data from shapy website script

---
+++ data/download_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
+
+read -p "Username:" username
+read -p "Password:" password
+
+username=$(urle $username)
+password=$(urle $password)
+
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=shapy&resume=1&sfile=shapy_data.zip' -O 'shapy_data.zip' --no-check-certificate --continue
+unzip shapy_data.zip
