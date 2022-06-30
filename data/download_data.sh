ae94ade2bd9e4bf490f37f4e0bcfda8f8b135f50
Lea Mueller <lea.mueller@tuebingen.mpg.de>
2022-06-30 22:17:53 +0200

    does not eacho password in download_data.sh

--- data/download_data.sh
+++ data/download_data.sh
@@ -2,7 +2,7 @@
 urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
 
 read -p "Username:" username
-read -p "Password:" password
+read -p "Password:" -s password
 
 username=$(urle $username)
 password=$(urle $password)
